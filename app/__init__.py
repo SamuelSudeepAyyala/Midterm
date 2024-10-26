a5616ecf5655059e157ce390c0c6a98691469658
SamuelSudeepAyyala <sa2826@njit.edu>
2024-10-25 21:09:49 -0400

    Plugin Architecture Added

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,40 @@
+import pkgutil
+import importlib
 from app.commands import CommandRegistry, Command
-from app.commands.add import AddCommand
-from app.commands.subtract import SubtractCommand
-from app.commands.multiply import MultiplyCommand
-from app.commands.divide import DivideCommand
+from app.plugins.add import AddCommand
+from app.plugins.subtract import SubtractCommand
+from app.plugins.multiply import MultiplyCommand
+from app.plugins.divide import DivideCommand
+from app.commands import CommandRegistry
+from app.commands import Command
 
 class App:
     def __init__(self):
         self.registry = CommandRegistry()
-
-        # Register available commands
         self.registry.register_command('add', AddCommand())
         self.registry.register_command('subtract', SubtractCommand())
         self.registry.register_command('multiply', MultiplyCommand())
         self.registry.register_command('divide', DivideCommand())
 
+    def load_plugins(self):
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue
     def run(self):
         print("Welcome to the REPL Calculator")
         while True:
             user_input = input("Enter command (add, subtract, multiply, divide) or 'exit' to exit: ").strip().lower()
 
             if user_input == 'exit':
+                print("Exiting....")
                 break
 
             if user_input in self.registry.commands:
@@ -38,7 +53,7 @@ class App:
             else:
                 print(f"Unknown command: {user_input}")
 
-# Running the REPL
+
 if __name__ == "__main__":
-    repl = REPL()
-    repl.run()
+    app = App()
+    app.start()
